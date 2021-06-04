26fc81b0520036ef67f97cdf15acdb0553ccb753
abdulloh031197 <abdulloh031197@mail.ru>
2021-06-04 19:14:54 +0500

    jask

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
   <div id="app">
     <!-- <router-view/> -->
     <div class="container">
-      <div class="row">
+      <div class="row" v-for="(item, index) in todoForm" :key="index">
         <div class="col-md-4">
-          <input type="text" v-model="input" id="" />
+          <div>
+            <input type="text" v-model="item.title" id="" />
+          </div>
         </div>
-        <div class="col-md-4">{{ input }}</div>
+        <div class="col-md-4">
+          <pre>{{ todoForm }}</pre>
+        </div>
       </div>
     </div>
   </div>
